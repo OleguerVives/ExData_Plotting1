cffe465d26c4e8c35c29297642b208981c6af1bd
OleguerVives <oleguer.vives.gil@gmail.com>
2019-02-07 18:29:46 +0100

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,27 @@
+##################################
+#####
+##### plot1.png
+#####
+##################################
+
+# Creating the directory to store the Dataset
+if(!file.exists("./data")){dir.create("./data")}
+
+# Downloading the course project data
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile="./data/Electronic power consumption.zip", method="curl")
+
+# Unzipping the downloaded Dataset
+unzip(zipfile="./data/Electronic power consumption.zip", exdir="./data")
+
+# Reading "household_power_consumption.txt" data set
+HPC <- read.table("./data/household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+# Subsetting '01/02/2007' and '02/02/2007' observations and Taking a look into HCP data set
+HPC <- subset(HPC, Date %in% c("1/2/2007", "2/2/2007"))
+head(HPC)
+
+png("plot1.png", width=480, height=480)
+with(HPC, hist(as.numeric(Global_active_power), col="red", main="Global Active Power", 
+               xlab="Global Active Power (kilowatts)"))
+dev.off()
